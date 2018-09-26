04da272216306dfbfa20871ed9bb241e60f0519a
Alan Thompson <pablosphere@gmail.com>
2018-09-26 09:43:06 -0400

    hackerchallenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,6 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. 
+//Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in 
+//place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,7 +19,17 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  for (var i=1; i < num; i++){
+      if (i % 15 == 0) {
+        console.log("fizzbuzz");
+      } else if (i % 3 == 0) {
+        console.log("fizz");
+       } else if (i % 5 == 0) {
+        console.log("buzz");
+      } else console.log(i);
+    }
+  }
+  fizzbuzz(1000)
 
-}
 
 module.exports = fizzbuzz;
