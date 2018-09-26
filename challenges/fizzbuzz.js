c21deb5ba9d95e7dd082e7e27474e1efe03c4341
Kyle Combs <combskyle@gmail.com>
2018-09-26 09:31:29 -0400

    finished fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,16 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  const output = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) { output.push('fizzbuzz'); }
+    else if (i % 5 === 0) { output.push('buzz'); }
+    else if (i % 3 === 0) { output.push('fizz'); }
+    else { output.push(i); }
+  }
+  return output;
 }
+
+console.log(fizzbuzz(16));
 
 module.exports = fizzbuzz;
