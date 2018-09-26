f2512170a829e5d7b322952f3b86e33b3ab9fd33
yongnicholaskim <yongnicholaskim@gmail.com>
2018-09-26 09:28:55 -0400

    complete

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,19 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      arr.push('fizzbuzz');
+    } else if (i % 5 === 0) {
+      arr.push('buzz');
+    } else if (i % 3 === 0) {
+      arr.push('fizz');
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
 }
 
 module.exports = fizzbuzz;
