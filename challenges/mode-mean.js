c8a5a926482ad20ccb90f207a7d97d08bf068e72
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-09-29 09:47:47 -0400

    Completed | Mode/Mean - TODO: Refactor

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,32 @@
 
 
 function modemean(array) {
-
-}
+    let len = array.length;
+    let dupCache = [];
+    let modes = [];
+    let mean;
+    let mode;
+  
+    mean = array.reduce((acc, cur, idx) => {
+      return acc + cur;
+    })
+  
+    array.reduce((acc, cur, idx) => {
+      acc = array[idx -1];
+      dupCache.push(acc)
+      if (dupCache.includes(cur)) {
+        modes.push(cur)
+      }
+    })
+  
+    mode = Math.max(...modes);
+    mean = mean / len;
+  
+    if (mean === mode) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 module.exports = modemean;
