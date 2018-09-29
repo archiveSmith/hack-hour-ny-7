f8bf94cd7e9ea52d4b10c199374c3943d9cda13c
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-09-29 10:38:03 -0400

    Completed | Reworked to address multiple modes. pt2

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,44 +10,33 @@
  */
 
 
-
 function modemean(array) {
-    let len = array.length;
-    let dupCache = [];
-    let modes = {};
-    let count = 0;
-    let mean;
-    let mode;
-  
-    mean = array.reduce((acc, cur, idx) => {
-      return acc + cur;
-    })
-  
-    array.reduce((acc, cur, idx) => {
-      acc = array[idx -1];
-      dupCache.push(acc)
-      if (dupCache.includes(cur)) {
-        modes[cur] = count++;
-      }
-    })
-  
-    Object.keys(modes).reduce((acc, cur) => {
-      if(modes[acc] > modes[cur]) {
-        mode = acc
-      } else if (modes[acc] < modes[cur]){
-        mode = cur;
-      } else if (modes[acc] === modes [cur]) {
-        mode = Math.max(...dupCache);
-      }
-    });
-  
-    mean = mean / len;
-  
-    if (mean === mode) {
-      return true;
-    } else {
-      return false;
+  let len = array.length;
+  let dupCache = [];
+  let modes = [];
+  let mean;
+  let mode;
+
+  mean = array.reduce((acc, cur, idx) => {
+    return acc + cur;
+  });
+
+  array.reduce((acc, cur, idx) => {
+    acc = array[idx -1];
+    dupCache.push(acc)
+    if (dupCache.includes(cur)) {
+      modes.push(cur)
     }
+  });
+
+  mode = Math.max(...modes);
+  mean = mean / len;
+
+  if (mean === mode) {
+    return true;
+  } else {
+    return false;
   }
+}
 
 module.exports = modemean;
