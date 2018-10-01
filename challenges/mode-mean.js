a4f173b4822e9b1bec3fe45b1db068661d66fe3b
yongnicholaskim <yongnicholaskim@gmail.com>
2018-09-30 20:24:12 -0400

    not completed

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,23 @@
  *
  */
 
-
 function modemean(array) {
+  var total = 0;
+  for (element of array) {
+    total += element;
+  }
+  var mean = Math.floor(total / array.length);
 
+  const counter = {};
+  for (let element of array) {
+    !Object.keys(counter).includes(element)
+      ? counter[element] = 1
+      : counter[element] += counter[element];
+  }
+  console.log(counter);
+  const mode = Object.keys(counter).reduce((acc, curr) => counter[acc] > counter[curr] ? acc : curr);
+
+  return (mean === mode);
 }
 
 module.exports = modemean;
