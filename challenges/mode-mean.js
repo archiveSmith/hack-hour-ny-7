e6b0fa42067c6c842c688a900f5d392379f674b9
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-10-02 14:02:07 -0400

    mode-mean update

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,11 +12,32 @@
 // mode is repeated more than any other
 
 function modemean(array) {
-function mean (){
-  return array.reduce(function(a, b) {
+if (mean === mode) {
+  return true
+}
+function mean(array){
+  value = array.reduce(function(a, b) {
     return a + b
   }) / array.length
+  mean = Math.floor(value);
+}
+function mode(array) {
+  var numList = {};
+  for(var i = 0; i < array.length; i++){
+      if(numList[array[i]] === undefined){
+          numList[array[i]] = 0;
+      }        
+          numList[array[i]] += 1;
+  }
+mostFreq = 0;
+mode;
+for (prop in numList) {
+  if (numList[prop] > mostFreq) {
+    mostFreq = numList[prop];
+    mode = prop;
+  }
 }
 }
+
 
 module.exports = modemean;
