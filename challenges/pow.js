29e9eb3e6c753ce5282180411d7baa0ab679020e
andrewcatalano <andrewtguerin@gmail.com>
2018-10-03 09:50:52 -0400

    recursive pow with negatives

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,17 +3,26 @@ where x is given as the base and y is given as the power.
  * Use recursion!
  */
 
-function pow(base, power) {
+function pow(base, power, isNeg = null){
     if(power === 0){
         return 1;
     }
-
+    if (power < 0){
+        isNeg = true;
+    }
+    if (power === - 1){
+        return 1/base;
+    }
+    
     if (power === 1){
         return base;
     }
-    return base * (pow(base, power - 1));
+    if (isNeg){
+        power *= - 1;
+        return 1 / (base * (pow(base, power - 1)));
+    } 
+    return (base * (pow(base, power - 1)))
 }
 
 
-console.log(pow(2, 0));
 module.exports = pow;
