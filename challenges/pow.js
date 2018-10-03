63c9f077dab67a22a61bbf3fd1ae201a2472ba72
Kyle Combs <combskyle@gmail.com>
2018-10-03 09:53:19 -0400

    pow done

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,23 @@
  */
 
 function pow(base, power) {
+  if (power < 0) return undefined;
+  if (power === 0) return 1;
+  if (power === 1) return base;
 
+  let count = 1;
+  let total = base;
+  
+  function calculate(current) {
+    console.log('calc running');
+    total = current * base;
+    console.log(total);
+    count++;
+    if (count < power) {
+       return calculate(total);
+    }
+  }
+  return calculate(total);
 }
 
 module.exports = pow;
