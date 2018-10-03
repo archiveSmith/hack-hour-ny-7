b3e7b30e7f04530bfdfce48a37ed1181f4c03651
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-03 09:51:25 -0400

    hack hour challenge completed

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,13 @@
  */
 
 function pow(base, power) {
-
+  if(power === 0){return 1;}
+  if(power === 1){
+    return base;
+  }else{
+    return base * pow(base, power - 1);
+  }
 }
 
+console.log(pow(8, 2));
 module.exports = pow;
