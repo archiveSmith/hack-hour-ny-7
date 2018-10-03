7e4616a7d33e4169dc28b9d604fb7b5e636f19b5
andrewcatalano <andrewtguerin@gmail.com>
2018-10-03 09:32:30 -0400

    recursive pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -1,9 +1,19 @@
-/* Write a function that calculates x^y, where x is given as the base and y is given as the power.
+/* Write a function that calculates x^y, 
+where x is given as the base and y is given as the power.
  * Use recursion!
  */
 
 function pow(base, power) {
+    if(power === 0){
+        return 1;
+    }
 
+    if (power === 1){
+        return base;
+    }
+    return base * (pow(base, power - 1));
 }
 
+
+console.log(pow(2, 0));
 module.exports = pow;
