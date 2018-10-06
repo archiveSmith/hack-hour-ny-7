d3678afd2ec0bed10fdf482e07e290aa2988dd6a
Matt Francis <matthewfrancis@Matthews-MacBook-Pro.local>
2018-10-06 09:47:37 -0400

    String Rotation Refactored

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -17,9 +17,8 @@ function isSubstring(s1, s2) {
 
 function stringRotation(s1, s2) {
   if(s1.length !== s2.length) return false;
-  let strArr = s1.split('');
-  strArr = strArr.concat(strArr).join('');
-  return isSubstring(strArr,s2.toLowerCase());
+  s1 += s1;
+  return isSubstring(s1.toLowerCase(),s2.toLowerCase());
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
