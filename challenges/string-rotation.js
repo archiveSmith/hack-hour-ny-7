2e1ba8a2fba7015459dc23cb401a13c7dba5413b
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-06 09:41:12 -0400

    COMPLETED | Over engineered 💩 code

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -15,8 +15,44 @@ function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
-function stringRotation(s1, s2) {
+/* It's very early and I over engineered this but should work, no? */
 
+function stringRotation(s1, s2) {
+  let subStringFunc = isSubstring(s1, s2);
+  let s1Arr = s1.split(' ');
+  let s2Arr = s2.split(' ');
+  let s1First = s1[0];
+
+  if (s1.length === s2.length) {
+    let rotated = []
+
+    for (let i = 0; i < s2.length; i++) {
+      if (s2[i] === s1First) {
+        for (let z = i; z < s2.length; z++) {
+          rotated.push(s2[z])
+          if (z === s2.length - 1) {
+            for (let x = 0; x < s2.length - i + 1; x++) {
+              rotated.push(s2[x])
+              console.log(rotated)
+            }
+          }
+        }
+      }
+    }
+    let rotatedString = rotated.join('')
+    if (s1 === rotatedString) {
+      return true;
+    } 
+    else {
+      return false;
+    }
+  }
+  else if (s2 === s1) {
+    return true;
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
