5418f4db26fb9aabf490e9f3a0b998a46d73a8e7
Kyle Combs <combskyle@gmail.com>
2018-10-17 09:21:00 -0400

    highestProduct take 1

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,9 @@
  */
 
 function highestProduct(array) {
-
+  let newArr = array.slice().sort((a, b) => a - b).reverse();
+  return newArr[0] * newArr[1] * newArr[2];
 }
 
-
+// console.log(highestProduct([2, 55, 1, 5, 6, 88, 3, -4]))
 module.exports = highestProduct;
