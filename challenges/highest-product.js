18213116b27e1fd940dc831f3a226f3e04f651d9
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-17 09:41:19 -0400

    First pass

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,22 @@
  */
 
 function highestProduct(array) {
+  let posArr = [];
+  let bigThree = [];
+  let negCount = 0;
 
+  for (let x = 0; x < 2; x++) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === Math.max(...array)) {
+        bigThree.push(array[i]);
+        array.splice(i, 1);
+      }
+    }
+  }
+
+  return bigThree.reduce((acc, cur, idx) => {
+    return acc *= cur;
+  });
 }
 
 
