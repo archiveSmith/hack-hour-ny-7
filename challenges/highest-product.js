0d3901184ca4b007d8eac37250c15c232e4288a2
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-17 09:23:08 -0400

    Completed Highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,13 @@
  */
 
 function highestProduct(array) {
-
+    array = array.sort((a, b) => a - b)
+    let lastThree = array.slice(-3);
+    return lastThree[0] * lastThree[1] * lastThree[2];
 }
 
 
 module.exports = highestProduct;
+
+console.log(highestProduct([5, 2, 4, 17, -5, 7, 3]));
+
