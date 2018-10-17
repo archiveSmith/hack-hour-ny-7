4bab3bd529098747c61219869d03b42959c4e83d
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-17 09:58:45 -0400

    Covered low negative edge cases

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,14 +3,24 @@
  */
 
 function highestProduct(array) {
-    if(!Array.isArray() || !array.length) return 0;
+    if(!Array.isArray(array) || !array.length) return 0;
 
     array = array.sort((a, b) => a - b);
+    let arrayNeg = 0;
+    let arrayPos = 0;
+    console.log(array);
+
     if (array[0] < 0 && array[1] < 0) {
-        return array[0] * array[1] * array[array.length-1];
+        arrayNeg = array[0] * array[1] * array[array.length-1];
+    } 
+
+    let lastThree = array.slice(-3);
+    arrayPos = lastThree[0] * lastThree[1] * lastThree[2];
+
+    if (arrayNeg > arrayPos) 
+        return arrayNeg;
     } else {
-        let lastThree = array.slice(-3);
-        return lastThree[0] * lastThree[1] * lastThree[2];
+        return arrayPos;
     }
 }
 
