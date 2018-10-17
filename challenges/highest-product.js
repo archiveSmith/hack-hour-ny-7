dff80c17299b9e2f94ca13c75f8e3e6ebfb85203
Anna <anna.brakowska91@gmail.com>
2018-10-17 09:20:53 -0400

    aesfdgf

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,10 @@
  */
 
 function highestProduct(array) {
-
+  array.sort((a, b) => a - b);
+  return array.splice(array.length - 3, 3).reduce((acc, curr) => {
+    return (acc *= curr);
+  });
 }
-
 
 module.exports = highestProduct;
