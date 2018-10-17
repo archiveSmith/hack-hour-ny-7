105a590e59f7596d67de75ed153c8d6446d282f4
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-17 09:28:28 -0400

    Handled Negative Edge Cases

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,13 +3,18 @@
  */
 
 function highestProduct(array) {
-    array = array.sort((a, b) => a - b)
-    let lastThree = array.slice(-3);
-    return lastThree[0] * lastThree[1] * lastThree[2];
+    array = array.sort((a, b) => a - b);
+    if (array[0] < 0 && array[1] < 0) {
+        return array[0] * array[1] * array[array.length-1];
+    } else {
+        let lastThree = array.slice(-3);
+        return lastThree[0] * lastThree[1] * lastThree[2];
+    }
 }
+
+console.log(highestProduct([-79, -12, -5, 5, 2, 6, 9, 10]))
 
 
 module.exports = highestProduct;
 
-console.log(highestProduct([5, 2, 4, 17, -5, 7, 3]));
 
