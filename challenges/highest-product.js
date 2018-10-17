7c72f856669b9deed02f265dd387fb711d19406b
yongnicholaskim <yongnicholaskim@gmail.com>
2018-10-17 09:43:15 -0400

    complete highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,21 @@
  */
 
 function highestProduct(array) {
+  let newArr = array.sort(function (a, b) {
+    if (a < b) { return 1; }
+    else if (a === b) { return 0; }
+    else { return -1; }
+  });
+
+  console.log(newArr);
+
+  let prod1 = newArr[0] * newArr[1] * newArr[2];
+  let prod2 = newArr[0] * newArr[newArr.length - 1] * newArr[newArr.length - 2];
+
+  console.log(prod1)
+  console.log(prod2)
+
+  return prod1 > prod2 ? prod1 : prod2;
 
 }
 
