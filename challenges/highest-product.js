b76ba6f43dd42cd8e78e29d6846329f63e5cd2b2
Kyle Combs <combskyle@gmail.com>
2018-10-17 09:54:57 -0400

    highestProd take 2

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,14 +3,17 @@
  */
 
 function highestProduct(array) {
+
+  if (array.length < 3 || !Array.isArray(array)) return 0;
+
   let sorted = array.slice().sort((a, b) => a - b).reverse();
   let prod = sorted.slice(0, 3);
 
-  prod.push(sorted.pop());
-  prod.push(sorted.pop());
+  if (array.length > 3 ) prod.push(sorted.pop());
+  if (array.length > 4 ) prod.push(sorted.pop());
 
   let first = prod[0] * prod[1] * prod[2];
-  let last = prod[0] * prod[3] * prod[4];
+  let last = prod[0] * prod[prod.length-1] * prod[prod.length-2];
 
   if (first > last) return first
   else return last;
