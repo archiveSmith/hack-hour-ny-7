9fc9593c105ce48ddd23a803c7511ffd141341b8
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-18 17:08:03 -0400

    UPDATED

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,54 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
-}
+    if (str.length === 0) return true;
+  
+    const stack = [];
+    const words = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]/g, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()
+      .split(' ');
+  
+    for (let word of words) {
+      const lastItemInStack = stack[stack.length - 1];
+      const reversedWord = word.split('').reverse().join('');
+  
+      if (lastItemInStack === reversedWord) {
+        stack.splice(stack.length - 1, 1);
+      } else {
+        stack.push(word);
+      }
+    }
+  
+    return stack.length === 0;
+  }
+  
+  function matchWord(str) {
+    const stack = [];
+    let word = '';
+  
+    for (let i = 0; i <= str.length; i++) {
+      const char = str[i];
+  
+      if (char && char.match(/[A-Za-z0-9]/)) {
+        word += char.toLowerCase();
+      } else if (word.length > 0) {
+        const reversedWord = word.split('').reverse().join('');
+        const lastItemInStack = stack[stack.length - 1];
+  
+        if (lastItemInStack === reversedWord) {
+          stack.splice(stack.length - 1, 1);
+        } else {
+          stack.push(word);
+        }
+  
+        word = '';
+      }
+    }
+  
+    return stack.length === 0;
+  }
 
 module.exports = matchWord;
