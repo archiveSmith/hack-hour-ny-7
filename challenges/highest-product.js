b2445507c02e2708fc8755bed0b5649f0756bdb4
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-10-18 19:59:18 -0400

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -9,21 +9,26 @@ function highestProduct(array) {
     return 0;
   } else if (array.length === 3) {
     return (arrProd = array.reduce((acc, cur) => {
-      return acc + cur;
+      return acc * cur;
     }));
   } else {
-    let posHigh = array.slice(array.length - 3, array.length);
-    let negHigh = array.slice(array.length - 1);
-    negHigh.push(array[0], array[1]);
-    let sumPos = posHigh.reduce((a, b) => a * b);
-    let sumNeg = negHigh.reduce((a, b) => a * b);
+    array = array.sort(function(a,b){
+      return a-b})
 
-    if (sumPos > sumNeg) {
+    let posHigh = array.slice(array.length - 3, array.length);
+    // let negHigh = array.slice(0,3);
+    let sumPos = posHigh.reduce((a, b) => a * b);
+    // let sumNeg = negHigh.reduce((a, b) => a * b);
+    let sumNeg2 = [];
+    sumNeg2.push(array[0] * array[1] * array[array.length-1])
+
+    if (sumPos > sumNeg2) {
       return sumPos;
     } else {
-      return sumNeg;
+      return sumNeg2;
     }
   }
 }
+
 
 module.exports = highestProduct;
