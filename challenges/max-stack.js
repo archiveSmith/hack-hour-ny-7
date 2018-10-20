a4d11cc25b79aaa51ae6c385dd453718cb401f3f
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-20 09:58:23 -0400

    Second Attempt

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,33 +8,40 @@
 
 function Stack() {
   // body...
-  this.storage = {};
-  this.index = 0;
+  this.length = 0
+  this.store = {}
+  this.max = {
+    idx: 0,
+    val: Number.NEGATIVE_INFINITY
+  };
 }
 
-Stack.prototype.push = (value) => {
-  this.storage[this.index] = value;
-  this.index+= 1;
-
-  return this.storage;
+Stack.prototype.push = function (value) {
+  if (value > this.max.val) {
+    this.max.idx = this.length;
+    this.max.val = value;
+  }
+  this.store[this.length] = value;
+  this.length++;
+  return this.length;
 }
 
-Stack.prototype.pop = (value) => {
-  let popstorage = this.storage[this.index-1];
-  delete this.storage[this.index]
-  this.index-= 1;
-
-  return popstorage;
+Stack.prototype.pop = function () {
+  if (this.length === 0) return undefined;
+  else {
+    let popped = this.store[this.length - 1]
+    if (popped === this.max.val) {
+      this.resetMax();
+    }
+    delete this.store[this.length - 1]
+    this.setNewMax();
+  }
+  this.length -= 1
+  return popped;
 }
 
-Stack.prototype.getMax = () => {
-  let currentStore = this.storage[this.index];
-  let compareArr = [];
-
-  compareArr.push(currentStore);
-
-  return Math.max(...compareArr)
-
+Stack.prototype.getMax = function () {
+  return this.length === 0 ? undefined : this.max.val;
 }
 
 module.exports = Stack;
