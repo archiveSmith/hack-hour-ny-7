a60335196e652f7d73b894098f16e53f109877c5
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-10-20 01:00:08 -0400

    Highes Prod Revision

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,32 +3,15 @@
  */
 
 function highestProduct(array) {
-  if (!Array.isArray(array)) {
-    return undefined;
-  } else if (array.length < 3) {
-    return 0;
-  } else if (array.length === 3) {
-    return (arrProd = array.reduce((acc, cur) => {
-      return acc * cur;
-    }));
-  } else {
-    array = array.sort(function(a,b){
-      return a-b})
+  if (!Array.isArray(array) || array.length < 3) return 0;
 
-    let posHigh = array.slice(array.length - 3, array.length);
-    // let negHigh = array.slice(0,3);
-    let sumPos = posHigh.reduce((a, b) => a * b);
-    // let sumNeg = negHigh.reduce((a, b) => a * b);
-    let sumNeg2 = [];
-    sumNeg2.push(array[0] * array[1] * array[array.length-1])
+  array.sort((a, b) => a - b);
 
-    if (sumPos > sumNeg2) {
-      return sumPos;
-    } else {
-      return sumNeg2;
-    }
-  }
+  const length = array.length;
+  const highThree = array[length - 1] * array[length - 2] * array[length - 3];
+  const lowTwo = array[0] * array[1] * array[length - 1];
+
+  return Math.max(highThree, lowTwo);
 }
-
 
 module.exports = highestProduct;
