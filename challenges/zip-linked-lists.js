943076171f8fbb1d4eea4737601b1e733396b9d6
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-19 21:43:40 -0400

    Completed Zip LL

---
+++ challenges/zip-linked-lists.js
@@ -0,0 +1,32 @@
+/* Merge two linked lists so that their nodes alternate. Let the first node of the zipped list be
+ * the first node of the first argument, if it exists.
+ * Implement the linked list using only a Node class. No need for a wrapper LinkedList class
+ *
+ * BONUS: Do this in place
+ */
+
+function Node(val) {
+  this.value = val;
+  this.next = null;
+}
+
+function zip(l1, l2) {
+  let nodeCounter = 0;
+  let headNode = new Node(l1[0]);
+  let nodeZipped = headNode;
+  let currentNode = nodeZipped;
+
+  for (let i = 0; i < l1.length; i++) {
+    if (i > 0) {
+      nodeZipped.next = new Node(l1[i]);
+      nodeZipped = nodeZipped.next;
+    }
+    nodeZipped.next = new Node(l2[i]);
+    nodeZipped = nodeZipped.next;
+  }
+  return headNode;
+};
+
+console.log(zip(["a", "b", "c", "d"], ["e", "f", "g", "h"]));
+
+module.exports = {Node: Node, zip: zip};
