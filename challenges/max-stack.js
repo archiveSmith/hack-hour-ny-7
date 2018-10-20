7fb0d7ad1262d5b756e8d7f633c37630b508b7d0
yongnicholaskim <yongnicholaskim@gmail.com>
2018-10-20 10:05:48 -0400

    complete

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,39 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.index = 0;
+  this.max = Math.max();
+  this.maxIndex = 0;
 }
+
+Stack.prototype.push = function (value) {
+  this.storage[this.index] = value;
+  this.index++;
+  if (value > this.max) {
+    this.max = value;
+    this.maxIndex = this.index-1;
+  }
+  return this.index;
+};
+
+Stack.prototype.pop = function () {
+  this.index--;
+  return this.storage[this.index];
+};
+
+Stack.prototype.getMax = function () {
+  return this.max;
+};
+
+// Stack.prototype.getMax = function () {
+//   let maxVal = Math.max();
+//   for (const key in this.storage) {
+//     if (this.storage[key] > maxVal) {
+//       maxVal = this.storage[key];
+//     }
+//   }
+//   return maxVal;
+// };
 
 module.exports = Stack;
