7cd599f469dee2224978c9aed178a1d8b17e5597
Matt Francis <matthewfrancis@Matthews-MacBook-Pro.local>
2018-10-20 10:09:47 -0400

    Refactored Max Stack using prototype chain

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -10,26 +10,29 @@ function Stack() {
   this.stack = {};
   this.length = 0;
   this.max = 0;
-  this.push = function(value) {
-    this.stack[this.length] = value;
-    if(value > this.max) this.max = value;
-    return ++this.length;
-  }
-  this.pop = function() {
-    this.length--;
-    let returnValue = this.stack[this.length];
-    delete this.stack[this.length];
-    if(returnValue === this.max) {
-      this.max = 0;
-      for(let i in this.stack) {
-        if(this.stack[i] > this.max) this.max = this.stack[i];
-      }
+}
+
+Stack.prototype.push = function(value) {
+  this.stack[this.length] = value;
+  if(value > this.max) this.max = value;
+  return ++this.length;
+}
+
+Stack.prototype.pop = function() {
+  this.length--;
+  let returnValue = this.stack[this.length];
+  delete this.stack[this.length];
+  if(returnValue === this.max) {
+    this.max = 0;
+    for(let i in this.stack) {
+      if(this.stack[i] > this.max) this.max = this.stack[i];
     }
-    return returnValue;
   }
-  this.getMax = function() {
-    if(this.length) return this.max;
-  }
+  return returnValue;
+}
+
+Stack.prototype.getMax = function() {
+  if(this.length) return this.max;
 }
 
 module.exports = Stack;
