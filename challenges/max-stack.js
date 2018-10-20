5c0471f89d1b2fb239ecef90c00ed31e04709d99
Kyle Combs <combskyle@gmail.com>
2018-10-20 09:37:01 -0400

    max-stack take 1

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,27 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = [];
+  this.index = 0;
+  this.maxValue = null;
+}
+
+Stack.prototype.push = function(value) {  
+  this.storage[this.index] = value;
+  this.index++;
+  if (value > this.maxValue) { this.maxValue = value };
+  return this.storage;
+};
+
+Stack.prototype.pop = function() {
+  this.index--;
+  const output = this.storage[this.index];
+  delete this.storage[this.index];
+  return output;
+}
+
+Stack.prototype.getMax = function() {
+  return this.maxValue;
 }
 
 module.exports = Stack;
