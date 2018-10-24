1d4ccd746dca48735ce515a0538101f44a17492f
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-24 10:03:23 -0400

    apple-stock challenge

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,7 +13,32 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
+  if(!stock_prices_yesterday || stock_prices_yesterday.length === 0){return 0;}
+  let min = stock_prices_yesterday[0]; 
+  let max = stock_prices_yesterday[0];
+  let profit;
 
+  for(let i = 0; i < stock_prices_yesterday.length; i++){
+    if(stock_prices_yesterday[i] > max){
+      max = stock_prices_yesterday[i];
+    }
+  }
+
+  profitArray = stock_prices_yesterday.splice(0, stock_prices_yesterday.indexOf(max));
+
+  for(let i = 0; i < profitArray.length; i++){
+    if(profitArray[i] < min){
+      min = profitArray[i];
+    }
+  }
+
+  profit = max - min; 
+
+  return profit > 0 ? profit : 0;
 }
+
+// const priceArray = [456, 234, 453, 324, 435, 543, 234, 565, 353, 453, 234, 345, 653, 657, 764, 234, 456, 454, 132, 345, 686, 567];
+
+// console.log(bestProfit(priceArray));
 
 module.exports = bestProfit;
