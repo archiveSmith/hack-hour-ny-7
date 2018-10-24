1793dca5b476eaf903bc1eab51a093d97d75444e
eternalee <one.eternity@gmail.com>
2018-10-24 09:38:23 -0400

    hh

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,50 @@ function Node(val) {
   this.next = null;
 }
 
+class LinkedList {
+  constructor(value) {
+    this.head = null;
+    this.length = 0;
+    this.addToHead(value);
+  }
+
+  addToHead(value) {
+    const newNode = {
+      value
+    }; // 1
+    newNode.next = this.head; // 2
+    this.head = newNode; // 3
+    this.length++;
+    return this;
+  }
+}
+const l1 = new LinkedList('first')
+  .addToHead('second')
+  .addToHead('third');
+
+const l2 = new LinkedList('first')
+  .addToHead('second')
+  .addToHead('third');
+
 function zip(l1, l2) {
+
 };
 
-module.exports = {Node: Node, zip: zip};
+/////////////////////////
+function Node(val) {
+  this.value = val;
+  this.next = null;
+}
+function zip(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let curr = new Node(l1.value);
+  curr.next = zip(l2, l1.next);
+  return curr;
+}
+
+module.exports = {
+  Node: Node,
+  zip: zip
+};
