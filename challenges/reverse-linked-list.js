311644fdd280c005002414f7366d5dd1275260b4
brandon6190 <brandon6190@outlook.com>
2018-10-24 09:38:26 -0400

    commit

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -7,12 +7,26 @@
  * Do it in place
  *
  */
+console.log("Hello from reverse-linked-list.js")
 
 function Node(value) {
     this.value = value;
     this.next = null;
 }
-
+/*TASK LIST:
+-create a function named reverseLinkedList with parameter: head
+    -declare a varieble named prev and assing it to null
+    -declare a varieble named curr and assing it the head param
+    -declare a varieble named next and leave it undeffined
+    -while curr has a value
+        -reassing next to be the value of the next prop in the curr variable
+        -reassing the vlaue of the next prop in the curr variable to be the value of prev
+        -reassing  the prev variable to be the value of curr
+    -end of while iteration
+    -reassign the head param to be the value of prev
+    -return the vlaue of head
+-end of reverseLinkedList
+*/
 function reverseLinkedList(head) {
 
 }
