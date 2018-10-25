a5f22b1c29ccf7467e6d86a16cc18288711d8156
brandon6190 <brandon6190@outlook.com>
2018-10-25 11:07:45 -0400

    apple-stock.js complete

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -11,9 +11,40 @@
  *
  *  Return 0 if no profit is possible OR if input is invalid.
  */
+// console.log("Hello from apple-stocks.js");
 
-function bestProfit(stock_prices_yesterday) {
+/* body of bestProfit function:
+  -if the spy param is not an array return 0
+  -assign infinity to lowestPrice variable
+  -assign 0 to maxProfit variable
+  -iterate through the spy param
+    -assign the current element in the spy param to currentPrice
 
+    -if the currentPrice is less than lowestPrice then...
+      -reassign the lowestPrice to the currentPrice
+    -else if the currentPrice minus the lowestPrice is greater than the maxProfit then...
+      -reassign the maxProfit to be the evaluation of the currentPrice minus the lowestPrice
+  -end of iteration
+  -return maxProfit
+*/
+// @param spy = stock_prices_yesterday
+function bestProfit(spy) {
+  if (!Array.isArray(spy)) return 0;
+  let lowestPrice = infinity;
+  let maxProfit = 0;
+  for (let i = 0; i < spy.length; i += 1) {
+    let currentPrice = spy[i];
+
+    if (currentPrice < lowestPrice) {
+      lowestPrice = currentPrice;
+    }
+    else if (currentPrice - lowestPrice > maxProfit) {
+      maxProfit = currentPrice - lowestPrice;
+    }
+  }
+  return maxProfit;
 }
+
+console.log(bestProfit(89));
 
 module.exports = bestProfit;
