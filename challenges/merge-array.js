536528f1a0be8be81493729a7a214060a1aea151
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-26 09:22:28 -0400

    update

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,21 @@
  */
 
 function mergeArrays(arr1, arr2) {
+  
+  let newArray = [];
+  let largeA = arr1.length < arr2.length ? arr1 : arr2;
+  let minNum = 0; 
+  let maxNum = 0; 
 
+  for(let i = 0; i < largeA.length; i++){
+
+  }
 }
+
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+
+console.log(mergeArrays(my_array, another_array)); 
+// * [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
 
 module.exports = mergeArrays;
