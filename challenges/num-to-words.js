0cbb16844716daaf023beb596b82d9c6f214e2f2
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-26 10:15:56 -0400

    num-to-words

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,33 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
+function numToWords(num, string = '') {
+  if (!num) { return num; }
+  let stringNum = num.toString();
+  let newString = string;
+  let ones = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  let tenUp = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+  let tens = ['', '', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
 
+  switch (stringNum.length) {
+
+    case (1):
+      return ones[num];
+
+    case (2):
+      if (stringNum[0] === '1') {
+        return tenUp[Number(stringNum[1])];
+      } else {
+        if (stringNum[1] === '0') {
+          return tens[Number(stringNum[0])];
+        } else {
+          newString += tens[Number(stringNum[0])];
+          return newString += numToWords(Number(stringNum.slice(1)), newString);
+        }
+      }
+  }
 }
+
+console.log(numToWords(65));
 
 module.exports = numToWords;
