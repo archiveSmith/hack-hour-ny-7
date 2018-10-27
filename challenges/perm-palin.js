fec00b6fe30b9b0510adbeffe80eb01ca59c9627
brandon6190 <brandon6190@outlook.com>
2018-10-27 10:58:50 -0400

    perm-palin.js update

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,18 @@
  * 	- permPalin('a') => true
  *
  */
+console.log("Hello from perm-palin.js");
+
+function checkPalin(str) {
+  const remove = /[\W_]/g;
+  const fixedString = str.toLowerCase().replace(remove, '');
+  const reversedString = fixedString.split('').reverse().join('');
+  const result = fixedString === reversedString;
+  return result;
+}
 
 function permPalin(str) {
-	
+
 }
 
 module.exports = permPalin;
