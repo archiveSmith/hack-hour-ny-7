a971018374ccf6dc7edcdbc1b5940e644c315674
eternalee <one.eternity@gmail.com>
2018-10-27 09:46:40 -0400

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,25 @@
  */
 
 function permPalin(str) {
-	
+	let s = str.split('').sort();
+  let obj = {};
+  for (let i=0; i<s.length; i++) {
+    if (!obj[s[i]]) {
+      obj[s[i]] = 1
+    } else {
+      obj[s[i]] = obj[s[i]]+1
+    }
+  }
+  // console.log(obj)
+  let odds = 0;
+  for (let x in obj) {
+    if (obj[x] % 2 != 0) odds++
+  }
+  if (odds % 2 == 0) {
+    return false
+  } else {
+    return true;
+  }
 }
 
 module.exports = permPalin;
