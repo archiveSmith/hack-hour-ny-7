a408e4117af49bcf5ae27f832d9b793c3bf95d58
andrewcatalano <andrewtguerin@gmail.com>
2018-10-27 10:01:50 -0400

    completed perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,29 @@
  */
 
 function permPalin(str) {
-	
+    //get string length
+    //if string is even number, all letters must appear even number of times 
+    //if string is odd, one letter appears once, all others appear even number of times
+
+    let chars = str.split('');
+    let counter = 0;
+    let len = chars.length;
+    let countObj = {};
+    for(let i = 0; i < chars.length; i++){
+        if(countObj[chars[i]]){
+            delete countObj[chars[i]];
+            counter--;
+        } else {
+            countObj[chars[i]] = 1;
+            counter++
+        }
+    }
+    if(len % 2 === 0){
+        return counter === 0;
+    } else {
+        return counter === 1;
+    }
+
 }
 
 module.exports = permPalin;
