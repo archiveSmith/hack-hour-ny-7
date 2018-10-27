e562c3003353b49d54fc17f43f0185285a750e62
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-10-27 13:10:44 -0400

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+    // if odd number there should be one left
+    // if even none left
+    let newArr = str.split("");
+    let charObj = {};
+    let counter = 0;
+    for (let i =0; i<newArr.length; i++) {
+        if (!charObj[newArr[i]]) {
+            charObj[newArr[i]] = 1;
+            counter++
+        } else if (charObj[newArr[i]]) {
+          delete charObj[newArr[i]] 
+          counter--
+        }
+    }
+    console.log(charObj)
+   if (newArr.length % 2 === 0 && counter === 0) {
+     return true
+   } else if (counter === 1) {
+     return true
+   } else {
+     return false
+   }
 }
+
+
 
 module.exports = permPalin;
