95155d197f4d897bd4c4c033bb6ef0f9a9b6b29c
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-27 10:01:53 -0400

    perm-palin challenge

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+  if(!str || str.length === 1){return true;}
+  let strArry = str.split(''); 
+  let strObj = {};
+  let oddCounter = 0;
+
+  for(let i = 0; i < strArry.length; i++){
+    if(!strObj[strArry[i]]){
+      strObj[strArry[i]] = 1;
+    }else{
+      strObj[strArry[i]]++;
+    }
+  }
+
+  for(let prop in strObj){
+    if(oddCounter > 1){
+      return false;
+    }
+
+    if(strObj[prop] % 2 !== 0){
+      oddCounter += 1;
+    }
+  }
+
+  return oddCounter <= 1 ? true : false;
 }
 
 module.exports = permPalin;
