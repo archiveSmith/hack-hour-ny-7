1373a7a0a2e0377f7405dcf254dd0396a4f4220a
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-29 09:25:15 -0400

    No new Changes

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,17 @@
  */
 
 function permPalin(str) {
-	
+  let cache = {};
+	for (let i = 0; i < str.length; i++) {
+    if (cache[i]) {
+      cache[i]++;
+    } else {
+      cache[i] = 0;
+    }
+  }
+  for (let keys of Object.keys){
+    if
+  }
 }
 
 module.exports = permPalin;
