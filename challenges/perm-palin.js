a175d2a4c67647d97b6a8c4addfe1602770ac1c0
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-29 09:30:08 -0400

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,39 @@
  */
 
 function permPalin(str) {
-	
-}
+    const stack = [];
+    const charArray = str.split('').sort();
+  
+    for (let char of charArray) {
+      if (stack[stack.length - 1] !== char) {
+        stack.push(char);
+      } else {
+        stack.pop();
+      }
+    }
+  
+    return stack.length <= 1;
+  }
+  
+//   function permPalin(str) {
+//     const table = {};
+//     let count = 0;
+  
+//     for (let char of str) {
+//       table[char] = !table[char];
+//     }
+  
+//     for (let char in table) {
+//       if (table[char] && ++count > 1) {
+//         return false;
+//       }
+//     }
+  
+//     return count <= 1;
+//   }
+  
+//   function permPalin(str) {
+//     return Object.values(str.split('').reduce((a, char) => ((a[char] = !a[char]), a), {})).reduce((a, value) => (value ? a + 1 : a), 0) <= 1;
+//   }
 
 module.exports = permPalin;
