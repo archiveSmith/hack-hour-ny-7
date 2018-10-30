a608f5b639d93c72c32e224baf2161f944f26529
andrewcatalano <andrewtguerin@gmail.com>
2018-10-30 09:58:29 -0400

    completed stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,20 @@
 
 
 function Stack() {
+    this.storage = {};
+    this.top = 0;
 
+    this.push = function(num){
+        this.storage[this.top++] = num;
+    }
+
+    this.pop = function(){
+        if(this.top > 0){
+            let temp = this.storage[--this.top];
+            delete this.storage[this.top];
+            return temp;
+        }
+    }
 }
 
 
@@ -14,7 +27,32 @@ function Stack() {
 
 
 function Queue() {
+  this.stack1 = new Stack();
+  this.stack2 = new Stack();  
+    
+  this.enqueue = function (item) {
+    this.stack1.push(item);
+  }
 
+  this.dequeue = function() {
+    if (this.stack2.top === 0) {
+
+      
+      while (this.stack1.top > 0) {
+        const neweststack1Item = this.stack1.pop();
+        this.stack2.push(neweststack1Item);
+      }
+
+      
+      if (this.stack2.top === 0) {
+        return undefined;
+      }
+    }
+    return this.stack2.pop();
+  }
 }
+
+
+
 
 module.exports = {Stack: Stack, Queue: Queue};
