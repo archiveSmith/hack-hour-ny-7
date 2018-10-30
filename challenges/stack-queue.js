63ae544aca56cb8b320101c1ab962cbc5a13689e
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-10-30 09:42:10 -0400

    Clean up

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -5,19 +5,22 @@
 
 function Stack() {    
     let storage = [];
+    
+    this.push = () => {
+        return storage.push.apply(storage, arguments);
+    };
 
-    this.push = function() {
-        storage.push.apply(storage, arguments);
-    };
-    this.pop = function() {
+    this.pop = () => {
         return storage.pop.apply(storage, arguments);
-    };
-    this.size = function() {
+    }
+
+    this.size = () => {
         return storage.length;
-    };
-    this.peek = function() {
+    }
+    
+    this.peek = () => {
         return storage;
-    };
+    }
 }
 
 
@@ -31,20 +34,20 @@ function Queue() {
     let stack1 = new Stack();
     let stack2 = new Stack();
 
-    this.eneque = function() {
+    this.eneque = () => {
         stack1.push.apply(stack1, arguments);
     };
-    this.dequeue = function() {
+    this.dequeue = () => {
         if (stack2.size() === 0) {
             while (stack1.size())
                 stack2.push(stack1.pop());
         }
         return stack2.pop();
     };
-    this.size = function(){
+    this.size = () => {
       return stack1.size() + stack2.size();
     };
-    this.peek = function() {
+    this.peek = () => {
         return stack2.peek();
     };
 }
