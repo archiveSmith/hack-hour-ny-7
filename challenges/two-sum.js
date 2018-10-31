11de0f036c2bf7fb312768fb66dd0306b2e6dfa8
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-10-31 09:46:42 -0400

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,16 @@
  */
 
 function twoSum(arr, n) {
-
-}
+    let match = false;
+    
+      for (let i=0;i<=arr.length;i++) {
+        for (let j=1; j<=arr.length;j++) {
+          if (arr[i] + arr[j] === n) {
+            match = true
+          }
+        }
+      }
+      return match
+    }
 
 module.exports = twoSum;
