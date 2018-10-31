fe6275875b570bb41054338ad79fedd83a984b65
Matt Francis <matthewfrancis@Matthews-MacBook-Pro.local>
2018-10-31 09:41:45 -0400

    two-sum solution

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,16 @@
  */
 
 function twoSum(arr, n) {
-
+  for(let i = 0; i < arr.length; i++) {
+    for(let x = 0; x < arr.length; x++) {
+      if(i !== x) {
+        if(arr[i] + arr[x] === n) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 module.exports = twoSum;
