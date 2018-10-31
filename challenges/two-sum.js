c05a53c3cfb9c14fd00939595f5ddaa5b921804f
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-31 09:40:09 -0400

    two-sum completed

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,21 @@
  */
 
 function twoSum(arr, n) {
+  if (!arr || !n || arr.length === 0){return false;}
 
+  for(let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length; j++){
+      if(i !== j && arr[i] + arr[j] === n){
+        return true;
+      }
+    }
+  }
+
+  return false
 }
+
+// const numArray = [12, 5, 6, 23, 15, 4, 3, 5, 13];
+
+// console.log(twoSum(numArray, 16))
 
 module.exports = twoSum;
