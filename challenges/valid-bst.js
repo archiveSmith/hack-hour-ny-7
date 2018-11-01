c23e08b37e9046f03f9e51773e8eac5978f565a9
Alan Thompson <pablosphere@gmail.com>
2018-11-01 10:25:55 -0400

    hackhour

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,16 +4,77 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
 
-function BinaryTree(val) {
+
+// function BinaryTree(val) {
+//     this.value = val;
+//     this.left = null;
+//     this.right = null;
+// }
+
+// function validBST(tree) {
+
+// }
+
+//module.exports = { BinaryTree: BinaryTree, validBST: validBST };
+
+function Node(val) {
     this.value = val;
     this.left = null;
     this.right = null;
 }
 
-function validBST(tree) {
-
+function BinarySearchTree() {
+    this.root = null;
 }
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+BinarySearchTree.prototype.push = function (val) {
+    var root = this.root;
+
+    if (!root) {
+        this.root = new Node(val);
+        return;
+    }
+
+    let currentNode = root;
+    let newNode = new Node(val);
+
+    while (currentNode) {
+        if (val < currentNode.value) {
+            if (!currentNode.left) {
+                currentNode.left = newNode;
+                break;
+            }
+            else {
+                currentNode = currentNode.left;
+            }
+        }
+        else {
+            if (!currentNode.right) {
+                currentNode.right = newNode;
+                break;
+            }
+            else {
+                currentNode = currentNode.right;
+            }
+        }
+    }
+}
+
+var bst = new BinarySearchTree();
+bst.push(3);
+bst.push(2);
+bst.push(4);
+bst.push(1);
+// bst.push(5);
+console.log(bst)
+
+function dfs(currentNode) {
+    if (currentNode) {
+        console.log(node.value);
+        dfs(node.left);
+        dfs(node.right);
+    }
+}
+
+console.log(currentNode)
