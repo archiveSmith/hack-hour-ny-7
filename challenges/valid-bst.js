d365e9d77b6d06cef7db45427626671c2cce8b28
Kyle Combs <combskyle@gmail.com>
2018-11-01 09:53:28 -0400

    validBST

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -13,7 +13,10 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
-
+    if (tree.left > tree.value || tree.right < tree.value) { return false };
+    if (tree.left !== null && tree.left <= tree.value) { return validBST(tree.left) };
+    if (tree.right !== null && tree.right >= tree.value) { return validBST(tree.right) };
+    return true;
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
