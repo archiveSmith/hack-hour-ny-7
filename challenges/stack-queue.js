98ceaa99766b84ceb7b95bf80ce92662272adbd0
Anthony Terruso <anthonyterruso@Anthonys-MacBook-Pro.local>
2018-11-01 09:25:55 -0400

    Stashed

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -3,26 +3,30 @@
  */
 
 
+let stack1 = new Stack();
+let stack2 = new Stack();
+
 function Stack() {    
-    let storage = [];
-    
-    this.push = () => {
-        return storage.push.apply(storage, arguments);
-    };
-
-    this.pop = () => {
-        return storage.pop.apply(storage, arguments);
-    }
-
-    this.size = () => {
-        return storage.length;
-    }
-    
-    this.peek = () => {
-        return storage;
-    }
+    this.storage = [];
+    this.index = 0;
 }
 
+
+Stack.push = () => {
+    return storage.push.apply(storage, arguments);
+};
+
+Stack.pop = () => {
+    return storage.pop.apply(storage, arguments);
+}
+
+Stack.size = () => {
+    return storage.length;
+}
+
+Stack.peek = () => {
+    return storage;
+}
 
 /**
 * Queue Class
@@ -30,26 +34,26 @@ function Stack() {
 
 
 function Queue() {
-
-    let stack1 = new Stack();
-    let stack2 = new Stack();
-
-    this.eneque = () => {
-        stack1.push.apply(stack1, arguments);
-    };
-    this.dequeue = () => {
-        if (stack2.size() === 0) {
-            while (stack1.size())
-                stack2.push(stack1.pop());
-        }
-        return stack2.pop();
-    };
-    this.size = () => {
-      return stack1.size() + stack2.size();
-    };
-    this.peek = () => {
-        return stack2.peek();
-    };
+    this.storage = {};
+    this.length = 0;
 }
+
+Queue.eneque = () => {
+    stack1.push.apply(stack1, arguments);
+};
+Queue.dequeue = () => {
+    if (stack2.size() === 0) {
+        while (stack1.size())
+            stack2.push(stack1.pop());
+    }
+    return stack2.pop();
+};
+
+Queue.size = () => {
+  return stack1.size() + stack2.size();
+};
+Queue.peek = () => {
+    return stack2.peek();
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
