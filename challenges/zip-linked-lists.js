c256130da1616ca56812acdecb29c631da05c97e
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-02 17:53:54 -0400

    zip linked lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,7 +11,9 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-  if (!l1 || !l2){return;}
+  if (!l1){return l2;}
+  if (!l2){return l1;}
+  
   let curr1 = l1;
   let curr2 = l2;
   let nextl1 = curr1.next;
