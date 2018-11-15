991ec332e0649018ebfd157c0c15df8216f43b82
brandon6190 <brandon6190@outlook.com>
2018-11-15 10:24:47 -0500

    repeat-number done

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,8 +10,32 @@
  *
  */
 
+ console.log('Hello from repeat-number')
+
+let myArray = [5, 2, 3, 5, 1, 10, 9];
+
 function repeatNumbers(array) {
+  let sortedArr = array.sort(function(a, b) {
+    return a - b;
+  });
+  
+  let { length } = sortedArr;
+
+  for (let i = 0; i < length; i+=1) {
+    let cur = sortedArr[i];
+    let next = sortedArr[i+1];
+
+    if (cur === next) {
+      return cur;
+    }
+    else {
+      continue;
+    }
+
+  }
 
 }
+
+console.log(repeatNumbers(myArray));
 
 module.exports = repeatNumbers;
