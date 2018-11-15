5609afc5c2153b067519e76513fdd463940e5ddf
Jonah Wilkof <jonahwilkof@Jonahs-MacBook-Pro.local>
2018-11-15 11:14:02 -0500

    repeat number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,15 @@
  */
 
 function repeatNumbers(array) {
-
+    let numList = {};
+    
+    for (let i = 0; i<array.length; i++) {
+      if (!numList.hasOwnProperty(array[i])) {
+        numList[array[i]] = 1
+      } else if (numList.hasOwnProperty(array[i])) {
+        return array[i]
+      }
+    }
 }
 
 module.exports = repeatNumbers;
