669e38e15833d22a2e18608ffa6dfcf7edfc10b2
eternalee <one.eternity@gmail.com>
2018-11-16 11:01:14 -0500

    merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,30 @@
 
 
 function mergeRanges(array) {
+  let result = [];
+  result.push(array[0])
 
+  for (let i=1; i<array.length; i++) {
+    let flag=false;
+    for (let j=0; j<result.length; j++) {
+      if (array[i][0] < result[j][1]) { //if 4 < 1... if 4<5
+        flag = true;
+        if (array[i][0] < result[j][0]) { //if 4 < 3
+          result[j][0] = array[i][0]; //change 3 to 4
+        } else {
+          if (array[i][1]>result[j][1]) {
+            result[j][1] = array[i][1];
+          } 
+        }
+      }  
+    } 
+      if (!flag) {
+        result.push(array[i]); 
+      }  
+  }
+  return result
 }
+array=[0];
+mergeRanges();
 
 module.exports = mergeRanges;
