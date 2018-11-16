cdad7115b282de5883b361add1e96066cf7a0a2c
brandon6190 <brandon6190@outlook.com>
2018-11-16 14:26:32 -0500

    merge-ranges.js

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,31 @@
  * Do not assume the ranges are in order
  */
 
+console.log('Hello from merge-ranges.js');
+
+var times = [[1, 3], [2, 5], [7,8]];
 
 function mergeRanges(array) {
+  let collector = [];
+  let { length } = array;
 
+  for (let i = 0; i < length-1; i+=1) {
+    let condensed = [];
+    let currArr = array[i];
+    let nextArr = array[i+1];
+
+    if (length % 2 === 1 && i === 0) {
+      collector.push(currArr);
+      continue;
+    }
+
+    condensed.push(currArr[0]);
+    condensed.push(nextArr[1]);
+    collector.push(condensed);
+  }
+  return collector;
 }
+
+console.log(mergeRanges(times));
 
 module.exports = mergeRanges;
