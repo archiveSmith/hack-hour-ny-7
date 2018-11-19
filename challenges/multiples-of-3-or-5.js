d45688db3f9065c55387fcde6f4eaa050ce3215a
discrete-projects <aterruso@gmail.com>
2018-11-19 09:34:37 -0500

    ATTEMPT

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,6 +7,17 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
+    for (let i = 1; i < 1000; i++) {
+    if (i % 3 === 0) {
+      sum += i;
+    } else if (i % 5 === 0) {
+      sum += i;
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      sum += i;
+    } else {
+      sum += 0;
+    }
+  }
 
   return sum;
 }
@@ -16,9 +27,21 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
+  for (let i = 1; i < z; i++) {
+    if (i % x === 0) {
+      sum += i;
+    } else if (i % y === 0) {
+      sum += i;
+    } else if (i % x === 0 && i % y === 0) {
+      sum += i;
+    } else {
+      sum += 0;
+    }
+  }
 
   return sum;
 }
+
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
