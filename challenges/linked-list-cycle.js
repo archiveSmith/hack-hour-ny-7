83432e183e2e8f306f3aa8e8e6fd5f8fb7fc64a0
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-19 10:14:03 -0500

    linked list cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -27,13 +27,34 @@
  *
  */
 
-var Node = function(value) {
+var Node = function (value) {
   this.value = value;
   this.next = null;
 }
 
 function hasCycle(head) {
+  let slow = head;
+  let fast = head;
 
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
-module.exports = {Node: Node, hasCycle: hasCycle}
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => true
+
+module.exports = { Node: Node, hasCycle: hasCycle }
