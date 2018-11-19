94025a316a9e182bca3b045b6bf1754630175d03
brandon6190 <brandon6190@outlook.com>
2018-11-19 11:06:05 -0500

    linked-list-cycle in progress

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,22 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
+  let prevVal;
 
+  for (let key in head) {
+
+  }
 }
+
+ var node1 = new Node('1');
+ var node2 = node1.next = new Node('2');
+ var node3 = node2.next = new Node('3');
+ var node4 = node3.next = new Node('4');
+ var node5 = node4.next = new Node('5');
+ hasCycle(node1); // => false
+ node5.next = node2;
+ hasCycle(node1); // => true
+
+// console.log(node2);
 
 module.exports = {Node: Node, hasCycle: hasCycle}
