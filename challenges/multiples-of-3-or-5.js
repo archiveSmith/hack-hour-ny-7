8d79e249fb4e2e7566b5d0a1fbaa13d24e5bec31
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-20 09:22:38 -0500

    multiples-of-3-or-5.js

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,6 +8,12 @@
 function sumMultiples3Or5Below1000() {
   let sum = 0;
 
+  for(let i = 0; i < 1000; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
+
   return sum;
 }
 
@@ -17,9 +23,17 @@ function sumMultiples3Or5Below1000() {
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
 
+  for(let i = 0; i < z; i++){
+    if(i % x === 0 || i % y === 0){
+      sum += i;
+    }
+  }
+
   return sum;
 }
 
+console.log(sumMultiples3Or5Below1000());
+console.log(sumMultiplesXOrYBelowZ(4, 6, 100));
 const objectToExport = {
   sumMultiples3Or5Below1000,
   sumMultiplesXOrYBelowZ,
