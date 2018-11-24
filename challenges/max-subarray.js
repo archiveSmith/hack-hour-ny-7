50c6c15c2c9b563bd51996adc1a4985bdf84e7cd
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-11-24 09:44:41 -0500

    max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,26 @@
  */
 
 function maxSubarray(arr) {
+  if (!arr) { return arr; }
+  const sumArr = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j <= arr.length; j++){
+      if(arr[i] === arr[j]){
+        console.log(arr[i]);
+        sumArr.push(arr[i] + arr[j]);
+      }else{
+        sumArr.push(sumArr[j - 2] + arr[j]);
+      }
+    }
+    // console.log(`---${i}---`, sumArr);
+  }
+
+  console.log(sumArr);
+  // return Math.max(...sumArr);
 }
+
+console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));//18
+// console.log(mxaxSubarray([15,20,-5,10]));//40
 
 module.exports = maxSubarray;
