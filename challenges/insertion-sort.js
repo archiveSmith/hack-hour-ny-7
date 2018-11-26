03f6672fef66f6d18496c121c474c8dda0547a22
Kyle Combs <combskyle@gmail.com>
2018-11-26 09:58:47 -0500

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    let value = array[i]
+    for (var j = i - 1; j > -1 && array[j] > value; j--) {
+      array[j + 1] = array[j]
+    }
+    array[j + 1] = value
+  }
 
+  return list
 }
 
 module.exports = insertionSort;
