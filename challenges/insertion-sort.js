a3f3a61413c7e03a746dac3e86b340766e19a0ce
eternalee <one.eternity@gmail.com>
2018-11-27 16:12:16 -0500

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,16 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let curr = array[i];
+    let j = i
+    while (array[j - 1] > array[j] && j > 0) {
+      array[j] = array[j - 1];
+      array[--j] = curr;
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
