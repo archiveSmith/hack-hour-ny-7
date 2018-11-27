37b4a0e123145d2409fc2fd21349802d2f59083e
discrete-projects <aterruso@gmail.com>
2018-11-27 09:47:45 -0500

    ATTEMPT 1

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
-}
+    array.forEach( (cur, idx) => {
+       while (idx - 1 >= 0 && array[idx - 1] > cur) {
+         array[idx] = array[idx - 1];
+         idx--;
+       }
+       array[idx] = cur;
+     });
+   
+     return array;
+   }
 
 module.exports = insertionSort;
