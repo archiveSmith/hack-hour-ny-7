a97b6aaa8e4cc21eb3d1633f04fdd4c2c119b912
filmboy3 <jonathanschwartz30@gmail.com>
2018-11-27 07:56:28 -0500

    completed insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,19 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i - 1; j > -1; j--) {
+      while (array[j+1] < array[j]){
+        let temp = array[j+1];
+        array[j+1] = array[j];
+        array[j] = temp;
+      }
+    }
+    
+  }
+  return array
 }
 
+
+console.log(insertionSort([2, 1, 3, 7, 4, 2, 9, 3, 8]));
 module.exports = insertionSort;
