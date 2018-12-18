4fbe722d244734fa8d25fcf7bbaafe18d5991a01
eternalee <one.eternity@gmail.com>
2018-12-18 10:22:15 -0500

    hackhour

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -26,4 +26,56 @@ function eachPermutation(arr, callback) {
 
 
 
+
+
+//******************************************* fellow solution
+function eachPermutation(arr, callback) {
+  function getPermutations(arr) {
+    if (arr.length <= 1) return [arr];
+
+    const first = [arr[0]];
+    const rest = arr.slice(1);
+
+    return getPermutations(rest).reduce((accum, perm) => {
+      for (let i = 0; i <= rest.length; i++) {
+        accum.push(
+          perm
+            .slice(0, i)
+            .concat(first)
+            .concat(perm.slice(i))
+        );
+      }
+
+      return accum;
+    }, []);
+  }
+
+  const perms = getPermutations(arr);
+  perms.forEach(perm => callback(perm));
+}
+
+// Backtracking & Decrease and Conquer O(n!)
+function eachPermutation(array, callback) {
+  return permute(0);
+
+  function permute(element) {
+    for (let i = element; i < array.length; i++) {
+      swap(array, i, element);
+      permute(element + 1);
+      swap(array, element, i);
+    }
+
+    if (element === array.length - 1) {
+      callback(array);
+    }
+  }
+
+  function swap(array, index1, index2) {
+    const temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+  }
+}
+
+
 module.exports = eachPermutation;
