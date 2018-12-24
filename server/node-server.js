4d2f52c3cd1be2b01182dfbeeb8dda42aea6a0b8
brandon6190 <brandon6190@outlook.com>
2018-12-24 13:42:37 -0500

    imported hackHourModule. You are now able to see results in the terminal

--- server/node-server.js
+++ server/node-server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const hackHourModule = require('../challenges/add-linked-list');
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-type': 'text/html'});
   fs.readFile('public/index.html', 'utf8', (err, data) => {
