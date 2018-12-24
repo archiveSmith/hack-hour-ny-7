1f746fe95b7e469641d31d341473100055b92690
brandon6190 <brandon6190@outlook.com>
2018-12-24 13:41:16 -0500

    Fixed some server issues and imported hackHourModule

--- server/express-server.js
+++ server/express-server.js
@@ -1,14 +1,17 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const hackHourModule = require('../challenges/add-linked-list');
+
 const app = express();
 
-app.use(bodyParser().json());
-app.use(bodyParser.urlencoded( { extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: true }));
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '../view')));
 
 app.listen(3001, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Server is listening on port 3001');
 });
