22785fc8eeff9bdca6bb1aab350602b8ba144305
neyser zana <neyser@neysers-MBP.lan1>
2018-12-29 18:38:44 -0500

    each-permutation.js

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -9,9 +9,6 @@ Permutations must NOT repeat any of the array elements.
 
 No return value is necessary.
 
-eachPermutation([1, 2, 3], function(perm) {
-  console.log(perm)
-});
 [ 1, 2, 3 ]
 [ 1, 3, 2 ]
 [ 2, 1, 3 ]
@@ -20,10 +17,34 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 3, 2, 1 ]
 */
 
-function eachPermutation(arr, callback) {
 
+// Backtracking & Decrease and Conquer O(n!)
+function eachPermutation(array, callback) {
+  // * return function with argument as 0 to act as index 
+  return permute(0);
+
+  function permute(element) {
+    // * we use element for the value of i and this is where it well start 
+    for (let i = element; i < array.length; i++) {
+      swap(array, i, element);
+      permute(element + 1);
+      swap(array, element, i);
+    }
+
+    if (element === array.length - 1) {
+      callback(array);
+    }
+  }
+
+  function swap(array, index1, index2) {
+    const temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+  }
 }
 
-
+console.log(eachPermutation([1, 2, 3], function(perm) {
+  console.log('PERM: ', perm)
+}));
 
 module.exports = eachPermutation;
