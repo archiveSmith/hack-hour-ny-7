52c883cf63f393f85c3994d0f4340a1da89be3ab
eternalee <one.eternity@gmail.com>
2019-01-16 13:50:57 -0800

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,35 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+//123, 132, 213, 231, 312, 321
 
+// function anagrams(string) {
+//   let result = [];
+//   if (string.length === 1) return [string]
+
+//   for (let i = 0; i < string.length; i++) {
+//     let char = string[i];
+//     let unused = string.slice(0, i) + string.slice(i + 1, string.length)
+//     let innerpermutations = anagrams(unused)
+
+//     for (let j = 0; j < innerpermutations.length; j++) {
+//       result.push(char + innerpermutations[j])
+//     }
+//   }
+//   return result;
+// }
+
+let result = [];
+function anagrams(string, x = '') {
+  if (!string) return result.push(x);
+
+  for (let i = 0; i < string.length; i++) {
+    x += string[i];
+    let unused = string.slice(0, i) + string.slice(i + 1, string.length)
+    anagrams(unused, x)
+    x = x.slice(0, x.length - 1)
+  }
 }
-
+anagrams('abc')
+console.log(result)
 module.exports = anagrams;
