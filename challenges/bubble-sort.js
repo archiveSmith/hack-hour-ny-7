d52e9a2901314bb2fc64185e65c012945a3f4977
eternalee <one.eternity@gmail.com>
2019-01-17 03:15:14 -0800

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,36 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
-function bubbleSort(array) {
-
+let swap = (arr, i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
+
+// function bubbleSort(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       if (array[j] > array[j + 1]) swap(array, j, j + 1)
+//     }
+//   }
+//   return array;
+// }
+
+let bubbleSort = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i - 1] > arr[i]) {
+        swap(arr, i - 1, i)
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+let test = [5, 2, 3, 1];
+// console.log(swap(test, 0, 3))
+console.log(bubbleSort(test))
 
 module.exports = bubbleSort;
