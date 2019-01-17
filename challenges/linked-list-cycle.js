b39ac712a410601bba4e687c8ddd2c8683c74ea2
Kyle Combs <combskyle@gmail.com>
2019-01-17 18:21:17 -0500

    moved files

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -45,18 +45,6 @@ function hasCycle(head) {
 
 }
 
-function hasCycleFast(head) {
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
-}
-
 // var node1 = new Node('1');
 // var node2 = node1.next = new Node('2');
 // var node3 = node2.next = new Node('3');
@@ -64,7 +52,7 @@ function hasCycleFast(head) {
 // var node5 = node4.next = new Node('5');
 //  // => false
 // // node5.next = node2;
-// // // hasCycle(node1);
-// console.log(hasCycleFast(node1)); // => true
+// // hasCycle(node1);
+// console.log(hasCycle(node1)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
