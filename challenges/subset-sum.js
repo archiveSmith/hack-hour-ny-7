80374c1a125debfd22ee969fed7f4cd7590928b7
Kyle Combs <combskyle@gmail.com>
2019-01-26 09:31:03 -0500

    testing subsetSum brute force

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,22 +8,22 @@
  subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-// function subsetSum(array, target) {
-
-//   const sets = [[]];
-
-//   for (let i = 0; i < array.length; i++) {
-//     for (let j = 0, length = sets.length; j < length; j++) {
-//       let temp = sets[j].concat(array[i]);
-//       sets.push(temp);
-//       let sum = temp.reduce((p,c) => { return p + c });
-//       if (sum === target) { return true }
-//     }
-//   }
-//   return "false";
-// };
-
 function subsetSum(array, target) {
+
+  const sets = [[]];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0, length = sets.length; j < length; j++) {
+      let temp = sets[j].concat(array[i]);
+      sets.push(temp);
+      let sum = temp.reduce((p,c) => { return p + c });
+      if (sum === target) { return true }
+    }
+  }
+  return "false";
+};
+
+function subsetSumRecursive(array, target) {
   if (!target) return true;
   if (!array.length) return false;
   
