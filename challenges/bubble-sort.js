9527d1dfcc2f2169d668d52b65b5a9c96e38706d
Kyle Combs <combskyle@gmail.com>
2019-01-28 09:10:05 -0500

    new bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -4,22 +4,36 @@
 
 
 
+// function bubbleSort(array) {
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     let swapped = false;
+//     for (let j = 0; j < i; j++) {
+//       if (array[j] > array[j+1]) {
+//         swapped = true;
+//         let temp = array[j+1];
+//         array[j+1] = array[j];
+//         array[j] = temp;
+//       }
+//     }
+//     if (!swapped) return array;
+//   }
+//   return array;
+// }
+
 function bubbleSort(array) {
-  for (let i = array.length - 1; i >= 0; i--) {
-    let swapped = false;
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j+1]) {
+  let swapped;
+  do {
+    swapped = false;
+    for(let i = 0; i < array.length; i++) {
+      if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
         swapped = true;
-        let temp = array[j+1];
-        array[j+1] = array[j];
-        array[j] = temp;
       }
     }
-    if (!swapped) return array;
-  }
+  } while(swapped);
   return array;
 }
 
-// console.log(bubbleSort([]));
+console.log(bubbleSort([12,23,1,555,3,11,4,2]));
 
 module.exports = bubbleSort;
