23e9ebb2d4a89b5d0d6074aded09922083bb85c8
Kyle Combs <combskyle@gmail.com>
2019-01-30 18:28:54 -0500

    zip-linked-list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,6 +10,7 @@ function Node(val) {
   this.value = val;
   this.next = null;
 }
+
 function zip(l1, l2) {
   if (!l1) return l2;
   if (!l2) return l1;
@@ -18,5 +19,29 @@ function zip(l1, l2) {
   curr.next = zip(l2, l1.next);
   return curr;
 }
+
+
+// iterative: 
+// function zip (l1, l2) {
+//   if (!l1) return l2;
+//   if (!l2) return l1;
+
+//   var head = l1;
+//   var temp = l1;
+  
+//   l1 = l1.next;
+  
+//   while (l2 && l1) {
+//     temp.next = l2;
+//     l2 = l2.next;
+//     temp = temp.next;
+//     temp.next = l1;
+//     l1 = l1.next;
+//     temp = temp.next;
+//   }
+  
+//   temp.next = l2 ? l2 : l1;
+//   return head;
+// }
 
 module.exports = { Node: Node, zip: zip };
