630d0c6fd014fb7519d317a1bd688454495caf33
Kyle Combs <combskyle@gmail.com>
2019-03-18 10:08:33 -0400

    bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -28,6 +28,26 @@ function binToDec(binary) {
   return output;
 }
 
+function binToDec(binary) {
+  const arr = binary.toString().split('')
+  const len = arr.length;
+
+  return arr.reduce((acc, curr, i) => acc += curr * Math.pow(2, len - 1 - i), 0);
+}
+
+console.log(binToDec(1101))
+
+function titleToNumber(s) {
+  const store = {A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20,U:21,V:22,W:23,X:24,Y:25,Z:26}
+  let output = 0;
+  let len = s.length;
+  const arr = s.split('')
+  for (let i = 0; i < len; i++) {
+      output += store[arr[i]] * Math.pow(26, len - 1 - i)
+  }
+  return output;
+};
+
 function decToBin(decimal) {
   let dec = decimal;
   let bin = [];
@@ -42,10 +62,10 @@ function decToBinEasy(decimal) {
   return Number(decimal.toString(2));
 }
 
-console.log(decToBin(357))
+console.log(decToBin(9))
 
-console.log(binToDec(101100101));
+// console.log(binToDec(101100101));
 
-console.log(decToBinEasy(357));
+// console.log(decToBinEasy(357));
 
 module.exports = binToDec;
