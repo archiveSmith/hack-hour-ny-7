91b32fb945b1021bebef6d01f8bb91276d395275
Kyle Combs <combskyle@gmail.com>
2019-03-18 10:08:40 -0400

    reformat

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -20,12 +20,18 @@
 //   return array;
 // }
 
+
+
+
+
+
+
 function bubbleSort(array) {
   let swapped;
   do {
     swapped = false;
-    for(let i = 0; i < array.length; i++) {
-      if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
+    for(let i = 0; i < array.length - 1 ; i++) {
+      if(array[i] > array[i + 1]) {
         [array[i], array[i + 1]] = [array[i + 1], array[i]];
         swapped = true;
       }
@@ -34,6 +40,21 @@ function bubbleSort(array) {
   return array;
 }
 
-console.log(bubbleSort([12,23,1,555,3,11,4,2]));
+
+function bubbleSort(array) {
+  for(let i = 0; i < array.length - 1; i+=1) {
+    let swapped = false;
+    for (let j = 0; j < array.length - 1 - i; j+=1) {
+      if (array[j] > array[j+1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) return array;
+  }
+  return array;
+}
+
+console.log(bubbleSort([12,23,1,555,3,11,4,7,2,1,5]));
 
 module.exports = bubbleSort;
