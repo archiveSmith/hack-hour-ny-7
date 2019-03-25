e69cadf4f8cbbadfe927d722c2f98bee93cdb21c
Kyle Combs <combskyle@gmail.com>
2019-03-24 22:47:58 -0400

    observed-pin

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -38,13 +38,40 @@ expectations = {
 }
 
 */
-
-
-
-
 function getPINs(observed) {
 
+  const dict = {
+    1: ['1', '2', '4'],
+    2: ['1', '2', '3', '5'],
+    3: ['2', '3', '6'],
+    4: ['1', '4', '5', '7'],
+    5: ['2', '4', '5', '6', '8'],
+    6: ['3', '5', '6', '9'],
+    7: ['4', '7', '8'],
+    8: ['5', '7', '8', '9', '0'],
+    9: ['6', '8', '9'],
+    0: ['8', '0']
+  }
+
+  if (!observed) return [];
+  const array = observed.split('')
+  console.log(array)
+  
+  function makePerms(arr, output = ['']) {
+    if (!arr.length) return output;
+    const newArr = [];
+    output.forEach(str => {
+      dict[arr[0]].forEach(num => {
+        newArr.push(str + num);
+      })
+    })
+    return makePerms(arr.slice(1), newArr);
+  }
+
+  return makePerms(array)
 }
+
+// console.log(getPINs('11'))
 
 
 module.exports = getPINs
