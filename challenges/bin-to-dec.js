dc4ab927d1173b22e28955d8cc891cc08c5e2fec
Kyle Combs <combskyle@gmail.com>
2019-04-16 07:57:02 -0400

    bin-to-dec refactor

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -36,6 +36,9 @@ function binToDec(binary) {
 
 console.log(binToDec(1101))
 
+// "Given a column title as appear in an Excel sheet, return its corresponding column number.
+// Example: (A -> 1, B -> 2, AA -> 27, AB -> 28)"
+
 function titleToNumber(s) {
   const store = {A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20,U:21,V:22,W:23,X:24,Y:25,Z:26}
   let output = 0;
@@ -46,6 +49,8 @@ function titleToNumber(s) {
   }
   return output;
 };
+
+console.log(titleToNumber('AAA'))
 
 function decToBin(decimal) {
   let dec = decimal;
